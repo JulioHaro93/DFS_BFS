9f03c7e8188e65fd93064809b804e32ddbd6e23f
Julio Haro <julioharocapetillo@gmail.com>
2024-05-31 16:43:43 -0600

    Kruskal-Prim completo

--- Main.py
+++ Main.py
@@ -7,16 +7,45 @@ import random
 
 grafo = Grafo()
         
-grafoMalla30 = grafo.grafoBarabasiAlbert(500,2)
+grafoMalla30 = Grafo().grafoDorogovtsevMendes(30, False)
 for arista in grafoMalla30.aristas:
-    arista.distancia = random.randint(0,100)
+    arista.distancia = random.randint(0,10000)
 krusk = pk.kruskal(grafoMalla30)
+
+grafo.generaGephi(krusk[0], "K-Chico")
+print("________________________________________")
+kruski = pk.kruskalInv(grafoMalla30)
+grafo.generaGephi(kruski[0], "KI-Chico")
+print("________________________________________")
+prim = pk.prim(grafoMalla30)
+#print(prim)
+grafo.generaGephi(prim[0],"prim-Chico")
+
+grafoMalla100 = Grafo().grafoDorogovtsevMendes(100, False)
+for arista in grafoMalla100.aristas:
+    arista.distancia = random.randint(0,10000)
+krusk = pk.kruskal(grafoMalla100)
+
+grafo.generaGephi(krusk[0], "K-Mediano")
+print("________________________________________")
+kruski = pk.kruskalInv(grafoMalla100)
+grafo.generaGephi(kruski[0], "KI-Mediano")
+print("________________________________________")
+prim = pk.prim(grafoMalla100)
+#print(prim)
+grafo.generaGephi(prim[0],"prim-Mediano")
+
+
+grafoMalla500 = Grafo().grafoDorogovtsevMendes(500, False)
+for arista in grafoMalla500.aristas:
+    arista.distancia = random.randint(0,10000)
+krusk = pk.kruskal(grafoMalla500)
 
 grafo.generaGephi(krusk[0], "K-Grande")
 print("________________________________________")
-kruski = pk.kruskalInv(grafoMalla30)
+kruski = pk.kruskalInv(grafoMalla500)
 grafo.generaGephi(kruski[0], "KI-Grande")
 print("________________________________________")
-prim = pk.prim(grafoMalla30)
+prim = pk.prim(grafoMalla500)
 #print(prim)
 grafo.generaGephi(prim[0],"prim-Grande")
